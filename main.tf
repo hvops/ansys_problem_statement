7fea1f09cae162d5dd34f87d864a15df46659c1f
hvops <harshvardhanlnjpit@gmail.com>
2025-02-10 11:42:09 +0530

    Declare missing aws_instance.web resource and associate with aws_eip

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  count             = 3
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.${count.index}.0/24"
-  availability_zone = element(["ap-south-1a", "ap-south-1b", "ap-south-1c"], count.index)
+  count                   = 3
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  availability_zone       = element(["ap-south-1a", "ap-south-1b", "ap-south-1c"], count.index)
   map_public_ip_on_launch = true
 }
 
@@ -28,14 +28,15 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "ansys_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet[0].id
-  security_groups = [aws_security_group.web_sg.name]
+  ami                         = "ami-0005b192e8e17af5f"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet[0].id
+  security_groups             = [aws_security_group.web_sg.name]
   associate_public_ip_address = true
 }
 
 resource "aws_eip" "web_ip" {
   instance = aws_instance.web.id
+  vpc  = true
 }
 
